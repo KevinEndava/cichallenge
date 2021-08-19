a6d3090727427742253cc1df3aaea56d09b19723
Kevin Puentes [Intern] <Kevin.Puentes@endava.com>
2021-08-19 17:12:13 -0500

    new tests included

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-'use strict';
+// 'use strict';
 
-const express = require('express');
+const express = require('./app.js');
 
 // Constants
 const PORT = 8000;
 const HOST = '0.0.0.0';
 
 // App
-const app = express();
-app.get('/', (req, res) => {
-    res.send('<h1>Hello world<h1>\n');
-});
+// const app = express();
+// app.get('/', (req, res) => {
+//     res.send('<h1>Hello world<h1>\n');
+// });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT,()=>{console.log( "listen on port",PORT)});
+
+
+// app.listen(PORT, HOST);
+// console.log(`Running on http://${HOST}:${PORT}`);
